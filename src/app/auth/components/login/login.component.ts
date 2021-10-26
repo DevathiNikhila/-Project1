6451d96028d279029e5ba728c97d993020fffebb
Nikhila <nikhiladbn@gmail.com>
2021-10-26 13:48:44 +0530

    first commit

---
+++ src/app/auth/components/login/login.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+  name!:string
+  //constructor() { }
+  constructor (private _router: Router){
+  }
+  onBackButtonClick():void{
+    this._router.navigate(['/ad-page']);
+  }
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/auth/components/register/register.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-register',
+  templateUrl: './register.component.html',
+  styleUrls: ['./register.component.css']
+})
+export class RegisterComponent implements OnInit {
+
+  constructor(private _router: Router) { }
+  onBackButtonClick():void{
+    this._router.navigate(['/ad-page']);
+  }
+  ngOnInit(): void {
+  }
+
+}
